20fc6baf7e8104502b2ad33fe1048601979a547e
david-hidayat <davidhidayat73@gmail.com>
2020-04-29 22:55:47 +0700

    Hode Tiitle Bar

--- app/src/main/java/hidayat/david/myapplication/MainActivity.java
+++ app/src/main/java/hidayat/david/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package hidayat.david.myapplication;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.view.Window;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -25,8 +26,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
-//        toolbar.getBackground().setAlpha(0);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
